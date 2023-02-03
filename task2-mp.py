a5b54c482dd07d246097b88120bd6c2f7d3037b3
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-02-02 22:04:40 -0800

    added hashes with same salt support, trimmed to words between 6 & 10

--- task2-mp.py
+++ task2-mp.py
@@ -1,27 +1,53 @@
 import bcrypt
 from nltk.corpus import words
-from multiprocessing import Pool, Event
+from multiprocessing import Process, Queue
 
 N_PARTS = 8
 word_dict = words.words("en")
+word_dict = [w for w in word_dict if len(w) >= 6 and len(w) <= 10]
 seg_size = len(word_dict)//N_PARTS
 word_lists = []
 
-def check_bcrypt(word_list):
-    hashed = b"$2b$08$J9FW66ZdPI2nrIMcOxFYI.qx268uZn.ajhymLP/YHaAsfBGP3Fnmq"
+# prints the current word it's checking every 1000 words.
+def check_bcrypt(word_list, found_queue, wanted_hashes, salt):
     for indx, passwd in enumerate(word_list):
-        if bcrypt.checkpw(passwd.encode('utf-8'), hashed):
-            print("found:", passwd)
+        curr_hashed = bcrypt.hashpw(passwd.encode('utf-8'), salt)
+        if found_queue.empty():
+            break
+        if curr_hashed in wanted_hashes:
+            print("found:", passwd, "for hash:", curr_hashed)
+            found_queue.get(timeout=5)
         if indx % 1000 == 0:
             print(passwd)
 
 if __name__ == '__main__':
+    # initialize chunks of data
     for i in range(N_PARTS-1):
         start, end = seg_size*i, seg_size*(i+1)
         word_lists.append(word_dict[start:end])
     word_lists.append(word_dict[end:])
 
-    with Pool(N_PARTS) as p:
-        p.map(check_bcrypt, word_lists)
+    wanted_hashes = [
+        b"$2b$08$J9FW66ZdPI2nrIMcOxFYI.qx268uZn.ajhymLP/YHaAsfBGP3Fnmq",
+        b"$2b$08$J9FW66ZdPI2nrIMcOxFYI.q2PW6mqALUl2/uFvV9OFNPmHGNPa6YC",
+        b"$2b$08$J9FW66ZdPI2nrIMcOxFYI.6B7jUcPdnqJz4tIUwKBu8lNMs5NdT9q"
+    ]
+    num_hashes = len(wanted_hashes)
+    salt = wanted_hashes[0][0:29]
+    print(len(word_dict))
+    print("finding words for shared salt", salt)
+    
+    # Fills queue with the num of hashes we want to find
+    # Proc removes one from queue if found. Once queue is empty, stop search.
+    found_queue = Queue()
+    for i in range(num_hashes):
+        found_queue.put_nowait(i)
+    
+    procs = []
+    for i in range(N_PARTS):
+        proc = Process(target=check_bcrypt, args=(word_lists[i], found_queue, wanted_hashes, salt))
+        procs.append(proc)
+        proc.start()
+    for proc in procs:
+        proc.join()
 
-# found: welcome
--- task2.py
+++ task2.py
@@ -12,16 +12,20 @@ def check_bcrypt(hashed):
         if indx % 1000 == 0:
             print(passwd)
 
-check_bcrypt(b"$2b$08$J9FW66ZdPI2nrIMcOxFYI.qx268uZn.ajhymLP/YHaAsfBGP3Fnmq")
+# check_bcrypt(b"$2b$08$J9FW66ZdPI2nrIMcOxFYI.qx268uZn.ajhymLP/YHaAsfBGP3Fnmq")
 
 # passwd = b's$cret12'
 
 # salt = bcrypt.gensalt()
 # hashed = bcrypt.hashpw(passwd, salt)
 # print(hashed)
-hashed = b'$2b$12$nDVcWso64xYb/79vyoJrbueOtrJOuGVtC2WN3vb3zssyA8vf4GJYe'
-passwd = 's$cret12'.encode('utf-8')
-if bcrypt.checkpw(passwd, hashed):
-    print("match")
-else:
-    print("does not match")
+hashed = b'$2b$08$J9FW66ZdPI2nrIMcOxFYI.qx268uZn.ajhymLP/YHaAsfBGP3Fnmq'
+# passwd = 's$cret12'.encode('utf-8')
+# if bcrypt.checkpw(passwd, hashed):
+#     print("match")
+# else:
+#     print("does not match")
+salt = hashed[0:29]
+hashed = bcrypt.hashpw(b"welcome", salt)
+print(hashed)
+# hashed.split(b"$", 3)[2])
