fd89d3f16d1eadf4d11b846c212cf126c3d25a71
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-02-07 14:58:19 -0800

    updated task 1

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import hashlib
+import time
 
 # part a
 def sha256(message):
@@ -12,17 +13,23 @@ string2 = s2_int.to_bytes(s2_int.bit_length(), 'big').decode('utf-8')
 print(sha256(string1).digest(), sha256(string2).digest())
 
 # part c
-def sha256modified(message):
-    return bin(int.from_bytes(hashlib.sha256(message.encode()).digest(), "big"))[8:51]
-i = 0
-hashdict = {}
-while (True):
-    hash = sha256modified(str(i))
-    if hash in hashdict.keys():
-        print(i, hashdict[hash], hash)
-        break
-    hashdict[hash] = i
-    i += 1
+def sha256modified(message, n):
+    return bin(int.from_bytes(hashlib.sha256(message.encode()).digest(), "big"))[:n]
+
+def collision_find(n):
+    i = 0
+    start_time = time.time()
+    hashdict = {}
+    while (True):
+        hash = sha256modified(str(i), n)
+        if hash in hashdict.keys():
+            print(f"{n}:", i, time.time()-start_time)
+            break
+        hashdict[hash] = i
+        i += 1
+
+for i in range(8, 51, 2):
+    collision_find(i)
 
 # found "2693682" and "1743751" share hash:
 # 1111101110000011001100110001011000000110000
